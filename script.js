27b97ca114ea9df8a7d68ac8dfc790196631bc81
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2024-07-24 21:54:13 -0300

    tela de login atuaalizada

---
+++ script.js
@@ -0,0 +1,11 @@
+function enviar () {
+    var mail = document.getElementById('email').value;
+    var sen =  document.getElementById('senha').value;
+
+    if(mail == "pessoa" & sen == "122") {
+     location.href = "https://www.linkedin.com/in/mateus-emanuel-4570201a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+
+    } else {
+     alert ('Senha errada');
+    }     
+}    
