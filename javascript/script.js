40c314f8e31330a5d69c8ed8652e823b8d65f41e
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2024-07-29 18:55:13 -0300

    atualizei a tela de login

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,45 @@
     var nome = document.getElementById('nome')
     var labelnome = document.getElementById('labelnome')
+    var validnome = false
 
     var usuario = document.getElementById('usuario')
     var labelusuario = document.getElementById('labelusuario')
+    var validusuario = false
 
     var senha = document.getElementById('senha')
     var labelsenha = document.getElementById('labelsenha')
+    var validsenha = false
 
     var consenha = document.getElementById('consenha')
     var labelconsenha = document.getElementById('labelconsenha')
-     
+    var validcosenha = false
 
-    function nomecorr() {
+    var error = document.getElementById('error')
+    var succes = document.getElementById('succes')
+
+    function nomecorr() { 
        if (nome.value.length <=2) {
         labelnome.setAttribute('style', 'color: red')
         labelnome.innerHTML = 'Minimo 3 carateres'
+        validnome = false
        
        } else {
         labelnome.setAttribute('style', 'color: #07F285')
         labelnome.innerHTML = 'Nome'
-       }
+        validnome = true
+        }
     }
 
     function usuariocorr() {
         if (usuario.value.length <=2) {
         labelusuario.setAttribute('style', 'color: red')
         labelusuario.innerHTML = 'Minimo 3 carateres'
-        
+        validusuario = false
+
         } else {
         labelusuario.setAttribute('style', 'color: #07F285')
         labelusuario.innerHTML = 'Usuario'
+        validusuario = true
         }
 
     }
@@ -38,11 +48,12 @@
         if (senha.value.length <=2) {
         labelsenha.setAttribute('style', 'color: red')
         labelsenha.innerHTML = 'Minimo 3 carateres'
-         
+        validsenha = false
+        
         } else {
         labelsenha.setAttribute('style', 'color: #07F285')
         labelsenha.innerHTML = 'Senha'
-
+        validsenha = true
         }
     }
 
@@ -50,15 +61,44 @@
         if (consenha.value.length <=2) {
         labelconsenha.setAttribute('style', 'color: red')
         labelconsenha.innerHTML = 'Minimo 3 carateres'
-        
+        validcosenha = false
+
         } else {
         labelconsenha.setAttribute('style', 'color: #07F285')
         labelconsenha.innerHTML = 'Senha'
+        validcosenha = true
         }   
     }
 
     function cadastrar () {
-     
+        if(validnome && validusuario && validsenha && validcosenha) {
+            var listauser = JSON.parse(localStorage.getItem('listauser') || '[]')
+
+            listauser.push(
+            {
+                nomecad: nome.value,
+                usercad: usuario.value,
+                senhacad: senha.value
+            }
+        )
+           
+          localStorage.setItem('listauser', JSON.stringify(listauser))
+           
+          succes.setAttribute('style', 'display: block')
+          succes.innerHTML = 'Cadastro com sucesso'
+          error.setAttribute('style', 'display: none')
+          error.innerHTML = ''
+          
+          window.location.href = 'https://mateus660.github.io/login-senha/html/index.html'
+
+
+        } else {
+          error.setAttribute('style', 'display: none')
+          error.innerHTML = 'cadastro inválido'
+          succes.setAttribute('style', 'display: block')
+          succes.innerHTML = ''
+        }      
+    
     }
 
     function enviar() {
